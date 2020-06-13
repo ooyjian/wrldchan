2f6df2cc22c162f88c6d27a9dc841c5aba0b68e1
Aelo14 <ouyujian14@gmail.com>
2020-06-13 17:40:38 +0800

    Add some new elements

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const publicDirPath = path.join(__dirname, '../public')
+const viewsDirPath = path.join(__dirname, '../templates/views')
+const partialsDirPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsDirPath)
+app.use(express.static(publicDirPath))
+hbs.registerPartials(partialsDirPath)
 
 app.get('', (req, res) => {
-    res.send('<h1>Cocksucka</h1>')
+    res.render('index')
 })
 
 app.listen(5000, () => console.log("App Running on Port 5000"))
