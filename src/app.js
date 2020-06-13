dce232bfac6731a3f7672800b64705ce9360b076
Aelo14 <ouyujian14@gmail.com>
2020-06-13 15:52:44 +0800

    Slight modification

--- src/app.js
+++ src/app.js
@@ -6,4 +6,4 @@ app.get('', (req, res) => {
     res.send('<h1>Cocksucka</h1>')
 })
 
-app.listen(3000, () => console.log("Success!"))
+app.listen(5000, () => console.log("App Running on Port 5000"))
