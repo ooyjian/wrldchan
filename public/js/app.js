21490a0f7c963283fd5e7183ce22f697573bd09e
Aelo14 <ouyujian14@gmail.com>
2020-06-15 10:59:09 +0800

    joife

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+const inputText = document.getElementById("userInput")
+const submitBtn = document.getElementById("submitBtn")
+
+submitBtn.addEventListener('submit', () => {
+    const text = inputText.textContent
+})
