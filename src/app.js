d2b4a30906562d31f1488532c5dc0bc0e605866d
Aelo14 <ouyujian14@gmail.com>
2020-06-16 18:06:34 +0800

    Add replies page

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 var bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { rawListeners } = require('process');
 
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
@@ -15,16 +16,28 @@ app.use(express.static(publicDirPath))
 app.use(bodyParser.urlencoded({ extended: true })); 
 hbs.registerPartials(partialsDirPath)
 
+var reply = ''
+
 app.post('', (req, res) => {
-    res.send(req.body)
+    reply = req.body.userInput
+    return res.redirect('/replies')
 })
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: "( ͡◉ ͜ʖ ͡◉)"
+    })
 })
 
 app.get('/coms', (req, res) => {
     res.render('coms')
+})
+
+app.get('/replies', (req, res) => {
+    res.render('replies', {
+        title: "Replies", 
+        reply
+    })
 })
 
 app.listen(5000, () => console.log("App Running on Port 5000"))
